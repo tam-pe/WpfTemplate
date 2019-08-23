d6999cf6591663153ab4265239085e3bc46c27d7
Nikita Chipchai <nikita.chipchai@softeq.com>
2019-08-23 12:15:02 +0300

    Add project files.

---
+++ WpfTemplate_45/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfTemplate_45
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
